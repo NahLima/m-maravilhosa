e84cb5a1e591ee7fa41d327d8b0c00e99d44bce8
Monaliza Vitor <65304621+monalizavitor@users.noreply.github.com>
2020-10-23 23:16:43 -0300

    Update maravilhosas-models.js

--- api/src/models/maravilhosas-models.js
+++ api/src/models/maravilhosas-models.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 let database = require('../data/data.json')
 
 const selectAllData = () => {
@@ -13,21 +15,31 @@ const selectDataById = (id) => {
 
 
 // criar uma nova maravilhosa sem repetir
-const insertData = (data) => {
-    const isFound = true
-    
-    database.filter(item => {
-        if (item.name !== data.name) {
-            return database.push(data)
-        }
-    })
+const insertData = (newMaravilhosa) => {
+    const maravilhosaFound = database.find(maravilhosa => maravilhosa.name == newMaravilhosa.name)
+
+    if (!newMaravilhosa.id) {
+        newMaravilhosa.id = Math.random().toString(36).substr(-8)
+    }
+
+    if (maravilhosaFound) {
+        return { error: { message: "Ops, registro duplicado" } }
+    } else {
+        fs.writeFileSync('./src/data/data.json', JSON.stringify([...database, newMaravilhosa]), 'utf8', (err) => {
+            if (err) {
+                console.log(err)
+            }
+        })
+
+        return { error: null }
+    }
 }
 
 // alterar cadastro maravilhosa
 const updateData = (id, update) => {
 
     const data = database.find(item => item.id == id)
-            const newData = database.splice(data, 1, update)
+    const newData = database.splice(data, 1, update)
 
 
 }
